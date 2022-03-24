6a7180bb9bfce228cfa2cda99018569db52d3739
fikri16f <fikrifirmansyah.moh@gmail.com>
2022-03-24 11:37:39 +0700

    navbar

---
+++ src/components/SidebarData.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Imports',
+        path: '/imports',
+        icon: <FaIcons.FaFileUpload />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Reports',
+        path: '/reports',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'nav-text'
+    },
+    
+]
---
+++ src/pages/Import.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Imports() {
+  return (
+    <div className="imports">
+        <h1>Imports</h1>
+    </div>
+  )
+}
+
+export default Imports
